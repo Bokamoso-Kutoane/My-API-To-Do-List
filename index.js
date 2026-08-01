02c28411d7d748fc1766e28d62f739c797efbe5d
Bokamoso-Kutoane <kutoanebokamoso505@gmail.com>
2026-08-01 13:28:24 +0200

    Stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const taskList = [ {"id": 1, "title": "Start Capstone", "done": true}, 
+    {"id": 2, "title": "Study java", "done": false},
+    {"id": 3, "title": "Revise PHP", "done": false} ];
 
 app.get('/', (req, res) => {
   res.send({ 
@@ -12,6 +15,21 @@ app.get('/', (req, res) => {
 
 app.get('/health', (req, res) => {
   res.send({"status": "ok"});
+});
+
+app.get('/tasks', (req, res) => {
+    res.send(taskList);
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const foundTask = taskList.find(task => task.id === Number(req.params.id));
+    if (foundTask){
+        res.send(foundTask);
+    } else {
+        res.status(404).json({error:`Task ${req.params.id} not found`});
+    }
+    
+    
 });
 
 app.listen(port, () => {
