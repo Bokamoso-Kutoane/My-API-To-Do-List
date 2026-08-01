555b4d970158abdc3362e91ce8c714e3573de0b1
Bokamoso-Kutoane <kutoanebokamoso505@gmail.com>
2026-08-01 14:20:03 +0200

    Stage 4: full CRUD

--- index.js
+++ index.js
@@ -46,6 +46,33 @@ app.post("/tasks", (req, res) => {
     res.status(201).json(newTask);
 });
 
+app.put("/tasks/:id", (req, res) => {
+    const foundTask = taskList.find(task => task.id === Number(req.params.id));
+    if (!foundTask){
+        return res.status(404).json({error:`Task ${req.params.id} not found`});
+    } 
+    if (req.body.title !== undefined) {
+        foundTask.title = req.body.title;
+    }
+    if (req.body.done !== undefined) {
+        foundTask.title = req.body.done;
+    }
+
+    res.status(200).json(foundTask);
+})
+
+app.delete("/tasks/:id", (req, res) => {
+    const index = taskList.findIndex(task => task.id === Number(req.params.id));
+
+    if (index === -1) {
+        return res.status(404).json({ error: `Task ${req.params.id} not found` });
+    }
+
+    taskList.splice(index, 1);
+
+    return res.sendStatus(204);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
